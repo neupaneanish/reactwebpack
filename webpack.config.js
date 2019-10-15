d0c352c985319f3d1165a85903797c4cb2484274
An Sh <neupaneanish@aol.com>
2019-10-15 15:13:00 +0545

    webpack.config error solved

--- webpack.config.js
+++ webpack.config.js
@@ -6,39 +6,41 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'index.js',
-    resolve: {
-      extensions: ['.jsx', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
           },
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-        },
-      ],
-    },
-    devServer: {
-      port: 6446,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-      }),
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
     ],
   },
+  devServer: {
+    port: 6446,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
