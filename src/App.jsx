b7ef72133430779f65ca117a82ff148eafbd3d1c
An Sh <neupaneanish@aol.com>
2019-10-15 14:29:59 +0545

    Configure ESLint Prettier with airbnb

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './main.scss';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <p>React Webpack Babel</p>
-    </div>
-  );
-};
-
+const App = () => (
+  <div>
+    <h1>Hello</h1>
+    <p>React Webpack Babel</p>
+  </div>
+);
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,40 @@ module.exports = {
   entry: './src/index.jsx',
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'index.js'
-  },
-  resolve: {
-    extensions: ['.jsx', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader'
+    filename: 'index.js',
+    resolve: {
+      extensions: ['.jsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
-          {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+      ],
+    },
+    devServer: {
+      port: 6446,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+    ],
   },
-  devServer: {
-    port: 3000
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
 };
